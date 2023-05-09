3c22020910f732deafd83967e0837c343339c93d
Anthony Nguyen <anguyenq@uw.edu>
2023-05-08 20:28:40 -0700

    Finish ints

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.util.Scanner;
-import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,20 +33,21 @@ public class Main {
         boolean valid = false;
         while (!valid) {
             System.out.println();
-            System.out.println(displayText);
+            System.out.print(displayText + ": ");
             input = IN.nextLine();
             switch (type) {
-                case name -> System.out.println();
+                case name -> valid = parseName(input);
                 case integer -> valid = parseInts(input);
-                case file -> System.out.println();
-                case password -> System.out.println();
+                case file -> valid = parseFile(input);
+                case password -> valid = parsePassword(input);
             }
         }
         return input;
     }
 
     /**
-     * Prompts for a password, then applies the proper salting and hashing and writes it to an output file.
+     * Prompts for a password, then applies the proper salting and hashing
+     * and writes it to an output file.
      */
     public static void getPassword() {
         Scanner scanner = new Scanner(System.in);
@@ -56,8 +56,10 @@ public class Main {
         boolean valid = false;
         while (!valid) {
             System.out.println();
-            System.out.println("Must be at least 8 characters, and at least one uppercase and lowercase,");
-            System.out.println("at least one special character and at least one digit.");
+            System.out.println("Must be at least 8 characters, and at least " +
+                    "one uppercase and lowercase,");
+            System.out.println("at least one special character and at least " +
+                    "one digit.");
             System.out.print("Password: ");
             pass = scanner.nextLine();
             valid = parsePassword(pass);
@@ -66,18 +68,17 @@ public class Main {
 
     /**
      * Checks if the input string contains the necessary password requirements
+     *
      * @param theInput input string
      * @return true/false if pattern matches
      */
     public static boolean parsePassword(final String theInput) {
-        return regex(".*[A-Z].*",theInput) &&
-                regex(".*[a-z].*",theInput) &&
-                regex(".*[^\\w\\d].*",theInput) &&
-                regex(".*\\d.*",theInput) &&
-                !regex(".{0,7}", theInput);
+        return regex(".*[A-Z].*", theInput) && regex(".*[a-z].*", theInput) && regex(".*[^\\w\\d].*", theInput) && regex(".*\\d.*", theInput) && !regex(".{0,7}", theInput);
     }
+
     /**
      * Prompts user for input and output filenames.
+     *
      * @return Array of 2 strings containing the filenames.
      */
     public static File[] getFiles() {
@@ -87,7 +88,8 @@ public class Main {
         boolean valid = false;
         while (!valid) {
             System.out.println();
-            System.out.println("Please enter the name of your input file including the extension (.txt only)");
+            System.out.println("Please enter the name of your input file " +
+                    "including the extension (.txt only)");
             System.out.print("Filename: ");
             String input = scanner.nextLine();
             valid = parseFile(input);
@@ -96,13 +98,15 @@ public class Main {
             } else {
                 inputFile = new File(input);
                 valid = inputFile.exists();
-                if (!valid) System.out.println("File does not exist in directory.");
+                if (!valid)
+                    System.out.println("File does not exist in directory.");
             }
         }
         valid = false;
         while (!valid) {
             System.out.println();
-            System.out.println("Please enter the name of your output file including the extension (.txt only)");
+            System.out.println("Please enter the name of your output file " +
+                    "including the extension (.txt only)");
             System.out.print("Filename: ");
             String output = scanner.nextLine();
             valid = parseFile(output);
@@ -117,6 +121,7 @@ public class Main {
 
     /**
      * Helper function to pattern match the filenames
+     *
      * @param theInput Input String
      * @return true/false if matches the pattern or not
      */
@@ -126,45 +131,28 @@ public class Main {
 
     /**
      * Prompts the user for two integers if they are in bounds.
+     *
      * @return Array of 2 integers.
      */
     public static int[] getInts() {
-        Scanner scanner = new Scanner(System.in);
-        boolean valid = false;
-        String first = "";
-        String second = "";
-        while (!valid) {
-            System.out.println();
-            System.out.println("Please enter your first int (Range " +
-                    "-2147483648 to 2147483647), commas are accepted if " +
-                    "properly placed");
-            first = scanner.nextLine();
-            valid = parseInts(first);
-        }
-        valid = false;
-        while (!valid) {
-            System.out.println();
-            System.out.println("Please enter your second int (Range " +
-                    "-2147483648 to 2147483647), commas are accepted if " +
-                    "properly placed");
-            second = scanner.nextLine();
-            valid = parseInts(second);
-        }
-        int firstInt = Integer.parseInt(first.trim().replace(",", ""));
-        int secondInt = Integer.parseInt(second.trim().replace(",", ""));
-        return new int[]{firstInt,secondInt};
+        String prompt = "Enter an int (Range -2147483648 to 2147483647, proper commas optional)";
+        final int[] ints = new int[2];
+        for (int i = 0; i < ints.length; i++)
+            ints[i] = Integer.parseInt(loopingPrompt(prompt, TEST.integer).trim().replace(",", ""));
+        return ints;
     }
 
     /**
      * Helper function to regex integer input
+     *
      * @param theInput String of integers
      * @return Boolean if matches the pattern (within bounds)
      */
     public static boolean parseInts(final String theInput) {
         String in = theInput.trim();
         boolean valid = false;
-        // "^-?\\d{1,10}$"
-        if (regex("^-?\\d{1,3}((,\\d{3}){0,3}|(\\d{3}){0,3})?$", in)) { //2147483647
+        if (regex("^-?\\d{1,3}((,\\d{3}){0,3}|(\\d{3}){0,3})?$", in)) {
+            //2147483647
             try {
                 Integer.parseInt(in.replace(",", ""));
                 valid = true;
@@ -177,6 +165,7 @@ public class Main {
 
     /**
      * Prompts the first and last name from user
+     *
      * @return A String array with the First and Last name on index 0 and 1.
      */
     public static String[] getName() {
@@ -187,8 +176,10 @@ public class Main {
         boolean valid = false;
         while (!valid) {
             System.out.println();
-            System.out.println("Input must be between 1 and 50 characters long.");
-            System.out.println("Only standard upper and lower case letters, dashes, and apostrophe's allowed");
+            System.out.println("Input must be between 1 and 50 characters " +
+                    "long.");
+            System.out.println("Only standard upper and lower case letters, " +
+                    "dashes, and apostrophe's allowed");
             System.out.print("First name: ");
             first = scanner.nextLine();
             valid = parseName(first);
@@ -197,8 +188,10 @@ public class Main {
         valid = false;
         while (!valid) {
             System.out.println();
-            System.out.println("Input must be between 1 and 50 characters long.");
-            System.out.println("Only standard upper and lower case letters, dashes, and apostrophe's allowed");
+            System.out.println("Input must be between 1 and 50 characters " +
+                    "long.");
+            System.out.println("Only standard upper and lower case letters, " +
+                    "dashes, and apostrophe's allowed");
             System.out.print("Last name: ");
             last = scanner.nextLine();
             valid = parseName(last);
@@ -207,7 +200,9 @@ public class Main {
     }
 
     /**
-     * Only allows Upper/Lowercase letters, apostrophe's and dashes, and length between 1 and 50.
+     * Only allows Upper/Lowercase letters, apostrophe's and dashes, and
+     * length between 1 and 50.
+     *
      * @param theInput The string to parse
      * @return Boolean if it fits the requirements
      */
@@ -217,6 +212,7 @@ public class Main {
 
     /**
      * Regex matcher helper function
+     *
      * @param theRegex The pattern string
      * @param theInput The string to be pattern matched
      * @return Boolean if the input string matches the pattern
