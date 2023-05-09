1ac24419ad0a28f2d92a0f039bdd3c0333df4c05
Anthony Nguyen <anguyenq@uw.edu>
2023-05-09 12:54:28 -0700

    Add extra tests for passwords

--- src/ParsePasswordTests.java
+++ src/ParsePasswordTests.java
@@ -27,5 +27,57 @@ public class ParsePasswordTests {
         Assertions.assertFalse(Main.parsePassword("Pass1!"));
     }
 
+    @Test
+    void testValidLength() {
+        Assertions.assertTrue(Main.parsePassword("aB!4567890"));
+    }
+
+    @Test
+    void testValidOneUpperCase() {
+        Assertions.assertTrue(Main.parsePassword("aB0!!!!!!!"));
+    }
+
+    @Test
+    void testValidOneDigit() {
+        Assertions.assertTrue(Main.parsePassword("aB!aBaBaB0"));
+    }
+
+    @Test
+    void testValidOnePunctuation() {
+        Assertions.assertTrue(Main.parsePassword("aB!4567890"));
+        Assertions.assertTrue(Main.parsePassword(".aB4567890"));
+        Assertions.assertTrue(Main.parsePassword("aB456,7890"));
+        Assertions.assertTrue(Main.parsePassword("aB4567890?"));
+    }
+
+    @Test
+    void testValidThreeLowerCase() {
+        Assertions.assertTrue(Main.parsePassword("aB!aaa7890"));
+    }
+
+    @Test
+    void testRejectLessThanTen() {
+        Assertions.assertFalse(Main.parsePassword("aB!456789"));
+    }
+
+    @Test
+    void testRejectNoUpperCase() {
+        Assertions.assertFalse(Main.parsePassword("ab!4567890"));
+    }
+
+    @Test
+    void testRejectNoDigit() {
+        Assertions.assertFalse(Main.parsePassword("aB!cdDfgGijk"));
+    }
+
+    @Test
+    void testRejectNoPunctuation() {
+        Assertions.assertFalse(Main.parsePassword("aBCcdDfgGijk0"));
+    }
+
+    @Test
+    void testRejectMoreConsecutiveLowerCase() {
+        Assertions.assertFalse(Main.parsePassword("!abccDDfgGijk0"));
+    }
 
 }
